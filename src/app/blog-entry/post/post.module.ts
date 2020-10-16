f2ea5793df25531b38e08ad6b827a96eacc7977b
James Lemieux <jimmylemieux9@gmail.com>
2020-10-15 20:03:38 -0400

    set up posts component

---
+++ src/app/blog-entry/post/post.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostComponent } from './post.component';
+
+@NgModule({
+  declarations: [PostComponent],
+  exports: [PostComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PostModule { }
