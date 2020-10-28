5b1bcdf4fa129df9d5eafd7abac3fedb7ab5cb31
JimmyLemieux <jimmylemieux9@gmail.com>
2020-10-27 20:47:47 -0400

    Showing Content on Blog Post

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var Prismic = require('prismic-javascript');
-
+const http = require('http');
 
 var apiEndpoint = "https://randblog.cdn.prismic.io/api/v2";
 const app = express();
@@ -10,11 +10,15 @@ app.get("/getDocuments", (req, res) => {
     Prismic.getApi(apiEndpoint, { req: req }).then(function(api) {
         return api.query(""); // An empty query will return all the documents
       }).then(function(response) {
-          console.log("Documents: ", response.results);
+          console.log("200 OK Got Documents from prismic");
           return res.json(response.results);
       }, function(err) {
         console.log("Something went wrong: ", err);
       });      
+});
+
+app.get("/getMovieQuote", (req, res) => {
+  
 });
 
 app.listen(port, () => {
